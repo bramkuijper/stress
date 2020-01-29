d734c7aa741b4d315d9532f7d3cf0c759fa096bc
alwk201 <alwk201@athena-l02.cm.cluster>
2020-01-29 11:46:03 +0000

    running things on carson

--- src/ibm/generate_sims.py
+++ src/ibm/generate_sims.py
@@ -12,17 +12,8 @@ sdmu = [ 0.02]
 #s_P2NP = [[0.01,0.02],[ 0.04,0.08], [ 0.1,0.2]]
 #s_NP2P = [[0.005,0.01], [ 0.02,0.04], [ 0.05, 0.1]]
 
-s_P2NP_pre = [ 0.1, 0.5, 0.9 ]
-s_NP2P_pre = [ 0.1, 0.5, 0.9 ]
-
-s_P2NP = []
-s_NP2P = []
-
-for s1_i in s_P2NP_pre:
-    s_P2NP += [[s1_i,s1_i]]
-
-for s1_i in s_NP2P_pre:
-    s_NP2P += [[s1_i,s1_i]]
+s_P2NP = [ 0.1, 0.5, 0.9 ]
+s_NP2P = [ 0.1, 0.5, 0.9 ]
 
 #cue_P = [ 0.5, 0.6, 0.7, 0.8, 0.9, 1.0 ]
 #cue_NP = [ 0.0, 0.2, 0.3, 0.4 ]
@@ -34,11 +25,10 @@ cue_NP = [ 0.0 ]
 ad = [ 0.5, 0.9, 1.0, 1.5 ]
 
 # power of the hormone level survival function
-aP = [ 0.5, 0.9, 1.0, 1.5 ]
+aP = [ 1.0 ]
 
 damage_decay = [ 1.0 ]
-damage_due_to_hormone = [ 0.25, 0.5, 1.0 ]
-stress_baseline_influx_pleio = [ 0.0, 0.5, 0.9 ]
+damage_due_to_hormone = [ 0.5, 0.75, 1.0 ]
 
 # number of replicates
 nrep = 3
@@ -112,9 +102,9 @@ for rep_i in range(0,nrep):
                                                                                         + str(mu_influx_i) + " " 
 
                                                                                         + str(sdmu_i) + " " 
-                                                                                        + str(s_P2NP_i[0]) + " " 
+                                                                                        + str(s_P2NP_i) + " " 
 
-                                                                                        + str(s_P2NP_i[1]) + " " 
+                                                                                        + str(s_P2NP_i) + " " 
                                                                                         + str(init_feedback_i) + " " 
 
                                                                                         + str(init_cue_influx_i) + " " 
